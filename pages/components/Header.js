21bf3f5a8938bd4c0c38cf0c5c38068a850fca19
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-04-26 13:42:35 +0200

    tailwind css added

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
 import { ConnectButton } from "web3uikit";
 
 export default function Header() {
-  return <ConnectButton />;
+  return (
+    <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+      <h1 className="py-4 px-4 font-bold text-3xl">Marketplace</h1>
+      <Link href="/">Marketplace</Link>
+      <ConnectButton />
+    </nav>
+  );
 }
