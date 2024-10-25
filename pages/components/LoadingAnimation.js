d43354f0a5e283047266b85c4bce9a1f0a3e7ec4
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-10-25 19:46:55 +0200

    LoadingAnimation fixed to not show scroll bar when shown

--- pages/components/LoadingAnimation.js
+++ pages/components/LoadingAnimation.js
@@ -1,10 +1,7 @@
 import {Loading} from "web3uikit";
 
 export const LoadingAnimation = () => (
-    <div className="flex justify-center items-center h-screen">
-        <Loading
-            size={80}
-            spinnerColor="#2E7DAF"
-        />
+    <div className="flex justify-center items-center fixed inset-0">
+        <Loading size={80} spinnerColor="#2E7DAF"/>
     </div>
 );
