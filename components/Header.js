1baf9ceb55e6a16455cd5d870d541ae4a606fdcf
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-11-18 12:29:01 +0100

    favicon aded

--- components/Header.js
+++ components/Header.js
@@ -95,7 +95,7 @@ export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
             <Link href="/" onClick={() => setMenuOpen(false)}>
-                <h1 className="py-4 px-4 font-bold text-3xl hover:text-blue-500">DecentMarkt</h1>
+                <h1 className="py-4 px-4 font-bold text-3xl hover:text-blue-500">♻️ DecentMarkt</h1>
             </Link>
 
             <div className="flex flex-row items-center">
